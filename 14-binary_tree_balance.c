c882fd8efef441f336e212bf13ae54304fa3853e
Mohamed8221 <s21511383eg@std.aou.edu.eg>
2024-03-28 07:51:09 +0200

    update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,44 +1,43 @@
 #include "binary_trees.h"
 
 /**
-* calc_balance - calculates the balance factor of a binary tree
-* @tree: pointer to the root node of the tree to calculate the balance factor
+* binary_tree_balance - measures the balance factor of a binary tree
+* @tree: pointer to the root node of the tree to measure the balance factor
 *
 * Return: balance factor of a binary tree. If tree is NULL, return 0.
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-int left_height = 0;
-int right_height = 0;
+int height_left = 0;
+int height_right = 0;
 
 if (tree == NULL)
 return (0);
 
-left_height = tree->left ? measure_height(tree->left) + 1 : 0;
-right_height = tree->right ? measure_height(tree->right) + 1 : 0;
+height_left = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+height_right = tree->right ? binary_tree_height(tree->right) + 1 : 0;
 
-return (left_height - right_height);
+return (height_left - height_right);
 }
 
 /**
-* measure_height - measures the height of a binary tree
+* binary_tree_height - measures the height of a binary tree
 * @tree: pointer to the root node of the tree to measure the height
 *
 * Return: height of a binary tree. If tree is NULL, return 0.
 */
-size_t measure_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 if (tree == NULL)
 return (0);
 else
 {
-/* compute the height of each subtree */
-size_t left_subtree_height = measure_height(tree->left);
-size_t right_subtree_height = measure_height(tree->right);
+size_t left_height = binary_tree_height(tree->left);
+size_t right_height = binary_tree_height(tree->right);
 
-/* use the larger one */
-if (left_subtree_height > right_subtree_height)
-return(left_subtree_height + 1);
-else return(right_subtree_height + 1);
+if (left_height > right_height)
+return (left_height + 1);
+else
+return (right_height + 1);
 }
 }
