223a947b7385c82718484160a82abd5700e3069d
Mohamed8221 <s21511383eg@std.aou.edu.eg>
2024-03-28 07:09:13 +0200

    update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,44 +1,44 @@
 #include "binary_trees.h"
 
 /**
- * calc_balance - calculates the balance factor of a binary tree
- * @tree: pointer to the root node of the tree to calculate the balance factor
- *
- * Return: balance factor of a binary tree. If tree is NULL, return 0.
- */
+* calc_balance - calculates the balance factor of a binary tree
+* @tree: pointer to the root node of the tree to calculate the balance factor
+*
+* Return: balance factor of a binary tree. If tree is NULL, return 0.
+*/
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left_height = 0;
-    int right_height = 0;
+int left_height = 0;
+int right_height = 0;
 
-    if (tree == NULL)
-        return (0);
+if (tree == NULL)
+return (0);
 
-    left_height = tree->left ? measure_height(tree->left) + 1 : 0;
-    right_height = tree->right ? measure_height(tree->right) + 1 : 0;
+left_height = tree->left ? measure_height(tree->left) + 1 : 0;
+right_height = tree->right ? measure_height(tree->right) + 1 : 0;
 
-    return (left_height - right_height);
+return (left_height - right_height);
 }
 
 /**
- * measure_height - measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
- *
- * Return: height of a binary tree. If tree is NULL, return 0.
- */
+* measure_height - measures the height of a binary tree
+* @tree: pointer to the root node of the tree to measure the height
+*
+* Return: height of a binary tree. If tree is NULL, return 0.
+*/
 size_t measure_height(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    else
-    {
-        /* compute the height of each subtree */
-        size_t left_subtree_height = measure_height(tree->left);
-        size_t right_subtree_height = measure_height(tree->right);
+if (tree == NULL)
+return (0);
+else
+{
+/* compute the height of each subtree */
+size_t left_subtree_height = measure_height(tree->left);
+size_t right_subtree_height = measure_height(tree->right);
 
-        /* use the larger one */
-        if (left_subtree_height > right_subtree_height)
-            return(left_subtree_height + 1);
-        else return(right_subtree_height + 1);
-    }
+/* use the larger one */
+if (left_subtree_height > right_subtree_height)
+return(left_subtree_height + 1);
+else return(right_subtree_height + 1);
+}
 }
